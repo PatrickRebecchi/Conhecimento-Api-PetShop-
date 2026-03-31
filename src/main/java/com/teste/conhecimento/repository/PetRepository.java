c55324b27d4452be01c2842064d45db6a30ef26c
Patrick <patrickrebecchi@hotmail.com>
2026-03-31 20:50:15 -0300

    feat: PetRepository CRIADO

---
+++ src/main/java/com/teste/conhecimento/repository/PetRepository.java
@@ -0,0 +1,7 @@
+package com.teste.conhecimento.repository;
+
+import com.teste.conhecimento.entity.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+}
